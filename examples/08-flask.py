be0eec0f9462513d68cbe2154b5b8abef51c7f7a
Jon Craton <jncraton@gmail.com>
2025-09-10 10:55:22 -0400

    Update example

--- examples/08-flask.py
+++ examples/08-flask.py
@@ -2,9 +2,15 @@ from flask import Flask
 from flask import send_file, request
 app = Flask(__name__)
 
+import random
+
 @app.route('/')
 def hello_world():
-    return "Hello, world"
+    return f"Hello, world {random.random()}"
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return f'User: {username}'
 
 if __name__ == '__main__':
     app.run(debug=True)
