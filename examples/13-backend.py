8c3117fd98c10b7753153afe2878fcfa0d97dff5
Jon Craton <jncraton@gmail.com>
2025-09-18 15:11:05 -0400

    Add 13-backend.py

---
+++ examples/13-backend.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import send_file, request
+app = Flask(__name__)
+
+import random
+
+@app.route('/')
+def root():
+    template = open('13-dice-roller.html').read()
+
+    max_val = int(request.args.get("size", 6))
+    value = random.randint(1, max_val)
+    
+    return template.replace("{result}", str(value))
+
+if __name__ == '__main__':
+    app.run(debug=True)
