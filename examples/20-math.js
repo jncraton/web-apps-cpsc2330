dce54394c76d9df0872fa47d74f94fb49bc057f6
Jon Craton <jncraton@gmail.com>
2025-10-15 10:57:36 -0400

    Add 20-math.js

---
+++ examples/20-math.js
@@ -0,0 +1,14 @@
+console.log(Math.PI)
+
+console.log(Math.round(Math.PI))
+console.log(Math.round(.5))
+console.log(Math.round(1.9))
+console.log(Math.round(-.9))
+
+function randInt(start, end) {
+  return Math.floor(Math.random() * (end - start + 1)) + start
+}
+
+Math.randInt = randInt
+
+console.log(Math.randInt(1, 10))
